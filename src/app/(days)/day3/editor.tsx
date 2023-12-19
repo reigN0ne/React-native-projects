b5f57ba828abaf743796bdcb02792c5671ba67a1
reigN0ne <python12345612@gmail.com>
2023-12-19 15:34:23 +0530

    Added Markdowns

--- src/app/(days)/day3/editor.tsx
+++ src/app/(days)/day3/editor.tsx
@@ -1,12 +1,78 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, ScrollView, TextInput, Pressable } from 'react-native'
+import Markdown from 'react-native-markdown-display';
+import MarkdownDisplay from '@components/day3/MarkdownDisplay';
+
+const template = `# Markdown Editor
+
+Hello **World**
+`;
 
 const EditorScreen = () => {
+    const [content, setContent] = useState(template);
+    const [tab, setTab] = useState('edit')
+
     return (
-        <View>
-            <Text>EditorScreen</Text>
+        <View style={styles.page}>
+            <View style={styles.tabsContainer}>
+                <Pressable onPress={() => setTab('edit')} style={[
+                    styles.tab, 
+                    {borderColor: tab === 'edit' ? 'green' : 'gray'}]}>
+                    <Text style={styles.tabText}>Edit</Text>
+                </Pressable>
+                <Pressable onPress={() => setTab('preview')} style={[
+                    styles.tab, 
+                    {borderColor: tab === 'preview' ? 'green' : 'gray'}]}>
+                    <Text style={styles.tabText}>Preview</Text>
+                </Pressable>
+            </View>
+            {tab === 'edit' ? (
+                <TextInput 
+                    value={content} 
+                    onChangeText={setContent}
+                    multiline
+                    numberOfLines={50} 
+                    style={styles.input}
+                />
+            ): (
+                <MarkdownDisplay>{content}</MarkdownDisplay>
+            )
+            }
+            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: 'whitesmoke',
+        flex: 1,
+        padding: 10,
+    },
+    input : {
+        backgroundColor: 'white',
+        textAlignVertical: 'top',
+        flex: 1,
+        borderRadius: 20,
+        paddingTop: 20,
+        fontSize: 16,
+    },
+    tabsContainer : {
+        flexDirection: 'row',
+        gap: 10, 
+        marginVertical: 10,
+    },
+    tab: {
+        padding: 10,
+        borderColor: 'gray',
+        flex: 1,
+        borderWidth: 2,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    tabText : {
+        fontFamily: 'InterSemiBold'
+    }
+})
 
 export default EditorScreen
--- src/app/(days)/day3/index.tsx
+++ src/app/(days)/day3/index.tsx
@@ -1,18 +1,26 @@
+import MarkdownDisplay from '@/components/day3/MarkdownDisplay'
 import { Link, Stack } from 'expo-router'
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const description = `
+# Markdown
+
+Integrate Markdown content in **React Native**
+`;
 
 const DayDetailsScreen = () => {
     return (
-        <View>
+        <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
             <Stack.Screen options={{
                 title: 'Day 3: Markdown'
             }}/>
-
+            <MarkdownDisplay>{description}</MarkdownDisplay>
             <Link href="/day3/editor" asChild>
                 <Button title="Go to Editor" />
             </Link>
-        </View>
+        </SafeAreaView>
     )
 }
 
